4d9b319a20bfd61c487e0eb2564d49c798a20ed8
Hannu Aarniala <hannu.aarniala@iki.fi>
2016-04-05 01:35:28 +0300

    Update styling for the about-section, inc. @media-queries. Also: SCSS
    Updated styling for the about section including @media-queries for type
    styling on the smaller screens. ALSO: removed the vendor prefixes, and
    changed to SCSS. Autoprefixer and css-minification through LibSass

--- _/js/off-canvas-effects.js
+++ _/js/off-canvas-effects.js
@@ -15,14 +15,15 @@ if ($('.site-canvas').hasClass('hide-main')) {
   // Do things on Main page Close
   $('.site-canvas').removeClass('hide-main');
   $('.site-canvas-inner').removeClass('opacity-main');
+  $('.main-toggle').removeClass('main-toggle-enable');
 } else {
   // Do things on Main page reveal
   $('.site-canvas').addClass('hide-main');
   // reduce opacity of the main content
   $('.site-canvas-inner').addClass('opacity-main');
-}
-
-//$('.site-wrapper').toggleClass('hide-main');
+  // toggle between the side and main content
+  $('.main-toggle').addClass('main-toggle-enable');
+  }
 }
 
 // Hide on ESC-key
